0ca7544ffbd52fa99d5ed602bea87908f3eb6b0a
Mauricio Chavez Olea <mauriciochavez@ciencias.unam.mx>
2018-03-18 20:45:51 -0600

    Práctica: Pila Ligada (COMPLETADA)

--- PilaLigada/src/ed/estructuras/lineales/Nodo.java
+++ PilaLigada/src/ed/estructuras/lineales/Nodo.java
@@ -1,26 +1,26 @@
 package ed.estructuras.lineales;
 
 class Nodo <E>{
-	private E datum;
-	private Nodo next;
+    private E datum;
+    private Nodo next;
 
-	public Nodo(E datum){
-		this.datum = datum;
-	}
-	public Nodo(E datum, Nodo next){
-		this.datum = datum;
-		this.next = next;
-	}
-	public void setDatum(E datum){
-		this.datum = datum;
-	}
-	public void setNext(Nodo next){
-		this.next = next;
-	}
-	public Nodo getNext(){
-		return next;
-	}
-	public E getDatum(){
-		return datum;
-	}
+    public Nodo(E datum){
+        this(datum,null);
+    }
+    public Nodo(E datum, Nodo next){
+        this.datum = datum;
+        this.next = next;
+    }
+    public void setDatum(E datum){
+        this.datum = datum;
+    }
+    public void setNext(Nodo next){
+        this.next = next;
+    }
+    public Nodo getNext(){
+        return next;
+    }
+    public E getDatum(){
+        return datum;
+    }
 }
--- PilaLigada/src/ed/estructuras/lineales/PilaLigada.java
+++ PilaLigada/src/ed/estructuras/lineales/PilaLigada.java
@@ -4,98 +4,107 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class PilaLigada<E> extends ColeccionAbstracta<E> implements IPila<E>{
-	private Nodo head;
-	private int size;
 
-	public PilaLigada(){
+    private Nodo head;
+    private int size;
 
-	}
+    public PilaLigada(){
+        this.head = null;
+        this.size = 0;
+    }
 
-	public PilaLigada(Nodo head,int size){
-		this.head = head;
-		this.size = size;
-	}
+    public PilaLigada(PilaLigada<E> copy){
+        this.head = copy.getHead();
+        this.size = copy.size();
+    }
 
-	private class Iterador implements Iterator<E>{
-		private Nodo pos;
+    private class Iterador implements Iterator<E>{
 
-		public Iterador(){
-			this.pos = head;
-		}
+        private Nodo pos;
 
-		public E next(){
+        public Iterador(){
+            this.pos = head;
+        }
+
+        public E next(){
             if(!hasNext())
-            	throw new NoSuchElementException("La iteracion no tiene mas elementos");
+                throw new NoSuchElementException("La iteracion no tiene mas elementos");
             E temp = (E) pos.getDatum();
-        	pos = pos.getNext();
+            pos = pos.getNext();
             return temp;
         }
 
         public boolean hasNext() {
-            return size;
+            if(pos==null)
+                return false;
+            return pos.getNext()!=null;
         }
 
         public void remove(){
-        	throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
-	}
+    }
 
-	/**
+    /**
      * Muestra el elemento al tope de la pila.
-	 * Devuelve <code>null</code> si está vacía.
+     * Devuelve <code>null</code> si está vacía.
      * @return Una referencia al elemento siguiente.
      */
-	public E mira(){
-		return (size==0) ? null : (E) head.getDatum();
-	}
+    public E mira(){
+        return (size==0) ? null : (E) head.getDatum();
+    }
 
-	/**
+    /**
      * Devuelve el elemento al tope de la pila y lo elimina.
-	 * Devuelve <code>null</code> si está vacía.
+     * Devuelve <code>null</code> si está vacía.
      * @return Una referencia al elemento siguiente.
      */
-	public E expulsa(){
-		if(size==0)
-			return null;
-		E temp = (E) head.getDatum();
-		head = head.getNext();
-		size--;
-		return temp;
-	}
+    public E expulsa(){
+        if(size==0)
+            return null;
+        E temp = (E) head.getDatum();
+        head = head.getNext();
+        size--;
+        return temp;
+    }
 
-	/**
+    /**
      * Agrega un elemento al tope de la pila.
      * @param e Referencia al elemento a agregar.
-	 * @throws IllegalArgumentException si se intenta agregar <code>null</code>.
+     * @throws IllegalArgumentException si se intenta agregar <code>null</code>.
      */
-	public void empuja(E a){
-		if(a==null)
-			throw new IllegalArgumentException("No es posible agregar null a la pila");
+    public void empuja(E a){
+        if(a==null)
+            throw new IllegalArgumentException("No es posible agregar null a la pila");
 
-		if(head==null)
-			head = new Nodo(a);
-		else{
-			Nodo temp = new Nodo(a, head);
-			head = temp;
-		}
+        if(head==null)
+            head = new Nodo(a);
+        else{
+            Nodo temp = new Nodo(a, head);
+            head = temp;
+        }
 
-		size++;
-	}
+        size++;
+    }
 
-	public Iterator<E> iterator(){
-		return new Iterador();
-	}
+    public Iterator<E> iterator(){
+        return new Iterador();
+    }
 
-	public boolean isEmpty(){
-		return size==0;
-	}
+    public boolean isEmpty(){
+        return size==0;
+    }
 
-	public int size(){
-		return size;	
-	}
+    public int size(){
+        return size;    
+    }
 
-	public boolean add(E e){
-		this.empuja(e);
-		return true;
-	}
+    public boolean add(E e){
+        this.empuja(e);
+        return true;
+    }
+
+    public Nodo getHead(){
+        return head;
+    }
 }
